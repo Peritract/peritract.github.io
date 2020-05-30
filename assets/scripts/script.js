7f48b81e7a55eaf4ef57b9ae87e1a10287e80989
Peritract <peritract@hotmail.co.uk>
2020-05-30 23:27:25 +0100

    base page

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,12 +1,26 @@
+// Check for previously stored user preferences
+
+let theme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+if (theme) {
+	document.body.setAttribute("data-theme", theme);	
+} else {
+	document.body.setAttribute("data-theme", "dark");
+}
+
+// Toggle light/dark theme on icon click
+
 document.querySelector(".theme-toggle").addEventListener("click", function (e) {
-	// Toggle light/dark theme on icon click
-	if (document.body.getAttribute("data-theme")) {
-		document.body.removeAttribute('data-theme');
+	if (document.body.getAttribute("data-theme") == "dark") {
+		document.body.setAttribute("data-theme", "light");
 		e.target.classList.remove("fa-moon");
 		e.target.classList.add("fa-sun");
+		localStorage.setItem("theme", "light");
 	} else {
-		document.body.setAttribute('data-theme', 'dark');
+		document.body.setAttribute("data-theme", "dark");
 		e.target.classList.add("fa-moon");
 		e.target.classList.remove("fa-sun");
+		localStorage.setItem("theme", "dark");
 	}
 });
+
